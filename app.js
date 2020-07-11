27a1286bae73807b9d3d833afc99a7d2624c0440
Olav Fosse <fosseolav@gmail.com>
2020-07-11 17:07:33 +0200

    Move blog model to models/blog.js

--- app.js
+++ app.js
@@ -3,15 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const {Blog} = require('./models/blog')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
