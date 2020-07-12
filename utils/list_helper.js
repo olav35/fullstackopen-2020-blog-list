9c30c21bf3cc8b0ea76c7b20d2b7fdc01cb1d279
Olav Fosse <fosseolav@gmail.com>
2020-07-12 15:59:43 +0200

    Add and test favoriteBlog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,7 +3,20 @@ const dummy = (blogs) => 1
 const totalLikes = (blogs) => 
   blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
+const favoriteBlog = (blogs) =>  {
+  let favoriteBlog = blogs[0]
+
+  blogs.forEach(blog => {
+    if(blog.likes > favoriteBlog.likes){
+      favoriteBlog = blog
+    }
+  })
+  
+  return favoriteBlog
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
