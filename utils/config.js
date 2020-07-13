6766d7085bde7b7a9e1a14564603aa8b61dba4d3
Olav Fosse <fosseolav@gmail.com>
2020-07-13 21:46:11 +0200

    Add INTERNET_SPEED environment variable

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 
 const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
+// Slow internet needs increase timeout
+const INTERNET_SPEED = process.env.INTERNET_SPEED || 'fast'
 
 module.exports = {
   MONGODB_URI,
   PORT: process.env.PORT,
-  SECRET: process.env.SECRET
+  SECRET: process.env.SECRET,
+  INTERNET_SPEED
 }
