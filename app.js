7df0e3adb56356e6280f64b7fd46c223eaf53c71
Olav Fosse <fosseolav@gmail.com>
2020-07-13 21:19:14 +0200

    Implement token based login

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const login = require('./controllers/login')
+
 mongoose.set('useCreateIndex', true)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -14,6 +16,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', login)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
